f7bc0963aef663b5009af71f33ca5014d69179da
Yasen Atanasov <y.atanasoff@gmail.com>
2024-11-25 15:35:17 +0200

    added getClosest to GameObject set

--- Runtime/RuntimeSet/GameObjectSet.cs
+++ Runtime/RuntimeSet/GameObjectSet.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 namespace HBM.Scriptable
@@ -5,5 +6,21 @@ namespace HBM.Scriptable
     [CreateAssetMenu(menuName = "HBM/Sets/GameObject")]
     public class GameObjectSet : RuntimeSet<GameObject>
     {
+        public GameObject GetClosest(Vector3 position)
+        {
+            GameObject closest = this.First();
+            float distance = Vector3.Distance(position, closest.transform.position);
+            foreach (var item in this)
+            {
+                var newDistance = Vector3.Distance(item.transform.position, position);
+                if (newDistance < distance)
+                {
+                    distance = newDistance;
+                    closest = item;
+                }
+            }
+
+            return closest;
+        }
     }
 }
