69c81c54a899db52c0a82001e36650f32845ca1b
Yasen Atanasov <y.atanasoff@gmail.com>
2024-11-25 15:53:51 +0200

    added distance to GetClosest

--- Runtime/RuntimeSet/GameObjectSet.cs
+++ Runtime/RuntimeSet/GameObjectSet.cs
@@ -6,10 +6,10 @@ namespace HBM.Scriptable
     [CreateAssetMenu(menuName = "HBM/Sets/GameObject")]
     public class GameObjectSet : RuntimeSet<GameObject>
     {
-        public GameObject GetClosest(Vector3 position)
+        public GameObject GetClosest(Vector3 position, out float distance)
         {
             GameObject closest = this.First();
-            float distance = Vector3.Distance(position, closest.transform.position);
+            distance = Vector3.Distance(position, closest.transform.position);
             foreach (var item in this)
             {
                 var newDistance = Vector3.Distance(item.transform.position, position);
