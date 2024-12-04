259914f962d055b67c81815c652e0e560caffef3
Yasen Atanasov <y.atanasoff@gmail.com>
2024-12-04 17:02:21 +0200

    fixed FloatVariable menuName

--- Runtime/Variable/Variables/FloatVariable.cs
+++ Runtime/Variable/Variables/FloatVariable.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace HBM.Scriptable
 {
-    [CreateAssetMenu(menuName = "HBM/HBM/Variables/Float")]
+    [CreateAssetMenu(menuName = "HBM/Variables/Float")]
     public class FloatVariable : Variable<float>
     {
         public void Change(float by)
